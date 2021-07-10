3c9000ae03456f685b36477a1a21e8c9ad9ac415
Diwana Nagales <diwanarae.nagales@benilde.edu.ph>
2021-07-10 22:36:26 +0800

    Added Contact Us

--- Assessment/Controllers/HomeController.cs
+++ Assessment/Controllers/HomeController.cs
@@ -7,6 +7,9 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
+using System.Net;
+using System.Net.Mail;
+
 namespace Assessment.Controllers
 {
     public class HomeController : Controller
@@ -20,6 +23,39 @@ namespace Assessment.Controllers
 
         public IActionResult Index()
         {
+            return View();
+        }
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult ContactUs(ContactUs record)
+        {
+            using (MailMessage mail = new MailMessage("contact.ccap@gmail.com", record.Email))
+            {
+                mail.Subject = record.Subject;
+
+                string message = "Hello, " + record.SenderName + "!<br/><br/>" +
+                    "Our team has received your inquiry. Here are the details: <br/><br/>" +
+                    "Contact Number: <strong>" + record.ContactNo + "</strong><br/>" +
+                    "Message:<br/><strong>" + record.Message + "</strong><br/><br/>" +
+                    "Please wait for our reply. Thank you!";
+                mail.Body = message;
+                mail.IsBodyHtml = true;
+
+                using (SmtpClient smtp = new SmtpClient())
+                {
+                    smtp.Host = "smtp.gmail.com";
+                    smtp.EnableSsl = true;
+                    NetworkCredential NetworkCred = new NetworkCredential("contact.ccap@gmail.com", "gboyabiwanikissleged24");
+                    smtp.UseDefaultCredentials = true;
+                    smtp.Credentials = NetworkCred;
+                    smtp.Port = 587;
+                    smtp.Send(mail);
+                    ViewBag.Message = "Inquiry sent.";
+                }
+            }
             return View();
         }
 
--- Assessment/Models/ContactUs.cs
+++ Assessment/Models/ContactUs.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,22 @@ namespace Assessment.Models
 {
     public class ContactUs
     {
+        [Display(Name = "Sender Name")]
+        [Required(ErrorMessage = "Required.")]
+        public string SenderName { get; set; }
+
+        [DataType(DataType.EmailAddress, ErrorMessage = "Invalid format.")]
+        [Required(ErrorMessage = "Required.")]
+        public string Email { get; set; }
+
+        [Display(Name = "Contact Number")]
+        public string ContactNo { get; set; }
+
+        [Required(ErrorMessage = "Required.")]
+        public string Subject { get; set; }
+
+        [Required(ErrorMessage = "Required.")]
+        [DataType(DataType.MultilineText)]
+        public string Message { get; set; }
     }
 }

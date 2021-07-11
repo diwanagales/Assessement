0ee83662c4a83e489a013a4765a113cc2b0a9b28
Diwana Nagales <diwanarae.nagales@benilde.edu.ph>
2021-07-11 16:59:04 +0800

    Fixed UI of Contact Us, still not sending email.

--- Assessment/Controllers/HomeController.cs
+++ Assessment/Controllers/HomeController.cs
@@ -25,6 +25,18 @@ namespace Assessment.Controllers
         {
             return View();
         }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
         public IActionResult ContactUs()
         {
             return View();
@@ -57,17 +69,6 @@ namespace Assessment.Controllers
                 }
             }
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
